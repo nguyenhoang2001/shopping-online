7127739779681d76375dddd8d384a9972ac47d2a
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-07-16 23:12:58 +0700

    starting

---
+++ my-app/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div classname='header'>
+            <img
+                className='header__logo'
+                src='https://english.mic.gov.vn/Upload_Moi/2020_vn/20200715-pg5.jpg'
+            />
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+            </div>
+            <div className="header__nav"></div>
+        </div>
+    )
+}
