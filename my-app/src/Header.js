b79bbf4d5992f6f47f219a42a5c994cf81742a16
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-07-16 23:23:26 +0700

    commitment made

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -8,10 +8,26 @@ export default function Header() {
                 className='header__logo'
                 src='https://english.mic.gov.vn/Upload_Moi/2020_vn/20200715-pg5.jpg'
             />
+            
             <div className="header__search">
-                <input type="text" className="header__searchInput" />
+                <input type="text" 
+                className="header__searchInput" 
+                />
             </div>
-            <div className="header__nav"></div>
+
+            <div className="header__nav">
+                <div className="header__option">
+
+                </div>
+
+                <div className="header__option">
+
+                </div>
+
+                <div className="header__option">
+
+                </div>
+            </div>
         </div>
     )
 }
